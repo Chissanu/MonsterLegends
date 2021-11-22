621d5a8c69b3dd72800ca5874212720ce2d7f605
Chissanu Kittipakorn <chissanuoak2@gmail.com>
2021-11-23 01:17:15 +0700

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from pydub import AudioSegment
-from pydub.playback import play
-
-song = AudioSegment.from_wav("C:\Users\WIndows10\Desktop\School Stuff\KMITL\Intro to Fundamental Programming lang\MonsterLegends\Assets\sound\click.wav")
-play(song)
-
